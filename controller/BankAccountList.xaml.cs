ad1f4594b843410315d4e7b96013b821bb73fee8
Łukasz Staniszewski <kontakt@lukaszstaniszewski.pl>
2021-02-13 21:24:10 +0100

    trashes

--- controller/BankAccountList.xaml.cs
+++ controller/BankAccountList.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace MoneyManagerToUniversity.controller
 {
     /// <summary>
-    /// Logika interakcji dla klasy BankAccount.xaml
+    /// Logika interakcji dla klasy BankAccountList.xaml
     /// </summary>
-    public partial class BankAccount : Window
+    public partial class BankAccountList : Page
     {
-        public BankAccount()
+        public BankAccountList()
         {
             InitializeComponent();
         }
